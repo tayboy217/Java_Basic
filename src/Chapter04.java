217842c1cbe802a4f942f770469df5ce39dcc0ac
tayboy217 <tayboy217@icloud.com>
2022-02-11 23:59:19 +0900

    [Update]Chapter4

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		int num1;
+     String str1;
+     String str2;
+     num1 = 217;
+     str1 = "Taichi Niina";
+     str2 = "Favorite Word";
+     System.out.println(str1);
+     System.out.println(str2);
+     System.out.println(num1);
+     
+	}
+}
