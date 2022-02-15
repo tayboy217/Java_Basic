8b97d23be671c3f6b79dcfa23c1af5f296bd37e4
tayboy217 <tayboy217@icloud.com>
2022-02-15 23:12:15 +0900

    Update all

---
+++ src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 9223372036854775807L;
+		System.out.println(l);
+		short s = 32767;
+		System.out.println(s);
+		double d = 1234567.89d;
+		System.out.println(d);
+		boolean b = true;
+        System.out.println(b);
+        
+        Integer i = 100;
+        System.out.println(i);
+        Float f = 1.4142f;
+        System.out.println(f);
+        Character c = 'う';
+        System.out.println(c);
+        
+        int[] array = { 0, 10, 20, 30, 40 };
+        System.out.println(array[3]);
+	}
+}
